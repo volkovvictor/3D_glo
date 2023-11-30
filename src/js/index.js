872835bd919b5587988b8de2486edd283d91bff4
Victor Volkov <vityan2504@gmail.com>
2023-11-30 17:14:05 +0300

    Написал функции открытия/закрытия меню и модального окна

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import timer from './modules/timer';
-import two from './modules/two';
+import menu from './modules/menu';
+import popup from './modules/popup';
 
 timer('2023.12.02');
-//two();
+menu();
+popup();
